a5581b31ed514b8c6d657b01c7cd18ea9ff86e46
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-29 17:52:29 -0500

    setup the authRouter file for the register and login posts

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -2,4 +2,57 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
+const { jwtSecret } = require('../config/secrets');
+
 const Users = require('../users/usersModel');
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = signToken(user);
+
+                res.status(200).json({
+                    token,
+                    message: `welcome ${user.username}`
+                });
+            } else {
+                res.status(401).json({ error: "invalid credentials"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+function signToken(user) {
+    const payload = {
+        id: user.id,
+        department: user.department,
+    };
+
+    const options = {
+        expiresIn: '1h'
+    };
+
+    return jwt.sign(payload, jwtSecret, options);
+}
+
+module.exports = router;
--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,3 @@
 module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'is it secret, is it safe'
+    jwtSecret: process.env.JWT_SECRET || 'Secret Stuff'
 };
